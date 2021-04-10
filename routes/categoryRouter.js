ceaa66404232492e94ecf6ab95c984e505aff104
Shaik,Sanaulla <Sanaulla.Shaik@Cerner.com>
2021-04-10 19:22:50 +0530

    Category get create update delete

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,6 +6,10 @@ const authAdmin = require("../middleware/authAdmin");
 router
   .route("/category")
   .get(categoryCtrl.getCategories)
-  .post(auth, authAdmin, categoryCtrl.createCategory);
+  .post(auth, authAdmin, categoryCtrl.createCategory)
+
+router.route("/category/:id")
+    .delete(auth,authAdmin, categoryCtrl.deleteCategory)
+    .put(auth, authAdmin, categoryCtrl.updateCategory);
 
 module.exports = router;
